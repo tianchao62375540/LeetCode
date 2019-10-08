facf85425a8140af66ff8f120f45acb7427c6556
tianchao <455433520@qq.com>
2019-10-08 17:49:47 +0800

    信号量demo 解决线程依次调用问题

---
+++ src/main/java/com/leetcode/java/add/SemaphoreDemo.java
@@ -0,0 +1,65 @@
+package com.leetcode.java.add;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Description:
+ * <p>
+ * ModelName:【】模块
+ *
+ * @author: TianChao
+ * Create at:  2019/10/8 16:30
+ * Company: 沈阳艾尔时代科技发展有限公司
+ * Copyright: (c)2018 AIR Times Inc. All rights reserved.
+ * @version: 1.0
+ */
+public class SemaphoreDemo {
+    private Semaphore smp = new Semaphore(2);
+    private Random rnd = new Random();
+
+    public static void main(String[] args) {
+        SemaphoreDemo demo = new SemaphoreDemo();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Runnable ra = demo.new TaskDemo("a");
+        Runnable rb = demo.new TaskDemo("b");
+        Runnable rc = demo.new TaskDemo("c");
+        Runnable rd = demo.new TaskDemo("d");
+        Runnable re= demo.new TaskDemo("e");
+        Runnable rf = demo.new TaskDemo("f");
+        Runnable rg = demo.new TaskDemo("g");
+        Runnable rh = demo.new TaskDemo("h");
+
+        executorService.execute(ra);
+        executorService.execute(rb);
+        executorService.execute(rc);
+        executorService.execute(rd);
+        executorService.execute(re);
+        executorService.execute(rf);
+        executorService.execute(rg);
+        executorService.execute(rh);
+    }
+    class TaskDemo implements Runnable {
+
+        private String id;
+
+        public TaskDemo(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            try {
+                smp.acquire();
+                System.out.println("Thread " + id + " is working");
+                Thread.sleep(rnd.nextInt(1000));
+                smp.release();
+                System.out.println("Thread " + id + " is over");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
