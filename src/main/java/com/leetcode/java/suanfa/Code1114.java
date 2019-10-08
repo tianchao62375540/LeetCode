e55e60cd37d32eb5a330d72e39d9c9433bae52eb
tianchao <455433520@qq.com>
2019-10-08 15:14:54 +0800

    多线程解题

--- src/main/java/com/leetcode/java/suanfa/Code1114.java
+++ src/main/java/com/leetcode/java/suanfa/Code1114.java
@@ -1,7 +1,9 @@
 package com.leetcode.java.suanfa;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 /**
  * Description:
@@ -16,7 +18,8 @@ import java.util.concurrent.Executors;
  */
 public class Code1114 {
     public static void main(String[] args) {
-        Foo foo = new Foo();
+        //Foo foo = new Foo();
+        FooThree foo = new FooThree();
         ExecutorService executorService = Executors.newCachedThreadPool();
         Runnable aR = () -> {
             System.out.println("one");
@@ -122,4 +125,74 @@ class Foo {
         }
 
     }
+}
+
+/**
+ * CountDownLatch 允许一个或者多个线程等待，知道其他线程完成为止
+ * 方法一直阻塞，直到由于调用{@link #countDown}方法而导致当前计数达到零为止，
+ * 此后*所有等待线程被释放，并且所有后续的* {@link #await调用await}立即返回。
+ * 这是一种一次性现象*-无法重置计数。如果您需要一个重置*计数的版本，请考虑使用{@link CyclicBarrier}。
+ */
+class FooTwo{
+    //声明两个 CountDownLatch变量
+    private CountDownLatch countDownLatch01;
+    private CountDownLatch countDownLatch02;
+    public FooTwo() {
+        //初始化每个CountDownLatch的值为1，表示有一个线程执行完后，执行等待的线程
+        countDownLatch01 = new CountDownLatch(1);
+        countDownLatch02 = new CountDownLatch(1);
+    }
+    public void first(Runnable printFirst) throws InterruptedException {
+        //当前只有first线程没有任何的阻碍，其余两个线程都处于等待阶段
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        //直到CountDownLatch01里面计数为0才执行因调用该countDownCatch01.await()而等待的线程
+        countDownLatch01.countDown();
+    }
+    public void second(Runnable printSecond) throws InterruptedException {
+        //只有countDownLatch01为0才能通过，否则会一直阻塞
+        countDownLatch01.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        //直到CountDownLatch02里面计数为0才执行因调用该countDownCatch02.await()而等待的线程
+        countDownLatch02.countDown();
+    }
+    public void third(Runnable printThird) throws InterruptedException {
+        //只有countDownLatch02为0才能通过，否则会一直阻塞
+        countDownLatch02.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+
+
+}
+
+class FooThree{
+    private Semaphore semaphore01;
+    private Semaphore semaphore02;
+    public FooThree() {
+        semaphore01 = new Semaphore(0);
+        semaphore02 = new Semaphore(0);
+    }
+    public void first(Runnable printFirst) throws InterruptedException {
+        //当前只有first线程没有任何的阻碍，其余两个线程都处于等待阶段
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        //直到CountDownLatch01里面计数为0才执行因调用该countDownCatch01.await()而等待的线程
+        semaphore01.release();
+    }
+    public void second(Runnable printSecond) throws InterruptedException {
+        //只有countDownLatch01为0才能通过，否则会一直阻塞
+        semaphore01.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        //直到CountDownLatch02里面计数为0才执行因调用该countDownCatch02.await()而等待的线程
+        semaphore02.release();
+    }
+    public void third(Runnable printThird) throws InterruptedException {
+        semaphore02.acquire();
+        printThird.run();
+    }
+
+
 }
